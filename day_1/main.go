2f43279807321093011b928d61b327a0093b28d0
Shrukan <shrukan@shrukanslab.xyz>
2023-12-01 00:18:09 -0700

    Day 1, Part 2

--- day_1/main.go
+++ day_1/main.go
@@ -2,19 +2,39 @@ package main
 
 import (
 	"bufio"
+	"fmt"
 	"os"
-	"strconv"
+	"sort"
+	"strings"
 )
 
-func numeric(c string) (int, bool) {
-	if num, err := strconv.Atoi(c); err == nil {
-		return num, true
-	}
-
-	return 0, false
+type Number struct {
+	idx   int
+	value int
 }
 
 func main() {
+	numeric_strings := map[string]int{
+		"1":     1,
+		"2":     2,
+		"3":     3,
+		"4":     4,
+		"5":     5,
+		"6":     6,
+		"7":     7,
+		"8":     8,
+		"9":     9,
+		"one":   1,
+		"two":   2,
+		"three": 3,
+		"four":  4,
+		"five":  5,
+		"six":   6,
+		"seven": 7,
+		"eight": 8,
+		"nine":  9,
+	}
+
 	file, err := os.Open("input.txt")
 	if err != nil {
 		panic(err)
@@ -24,21 +44,29 @@ func main() {
 
 	scanner := bufio.NewScanner(file)
 	for scanner.Scan() {
-		first, last := 0, 0
-		first_found := false
+		numbers_found := []Number{}
 
 		line := scanner.Text()
-		for _, c := range line {
-			if num, is_num := numeric(string(c)); is_num {
-				last = num
-				if !first_found {
-					first = num
-					first_found = true
-				}
+		for str, val := range numeric_strings {
+
+			first_idx := strings.Index(line, str)
+
+			if first_idx >= 0 {
+				numbers_found = append(numbers_found, Number{first_idx, val})
+			}
+
+			last_idx := strings.LastIndex(line, str)
+
+			if last_idx >= 0 {
+				numbers_found = append(numbers_found, Number{last_idx, val})
 			}
 		}
 
-		sum += first*10 + last
+		sort.SliceStable(numbers_found, func(i, j int) bool {
+			return numbers_found[i].idx < numbers_found[j].idx
+		})
+
+		sum += numbers_found[0].value*10 + numbers_found[len(numbers_found)-1].value
 
 	}
 
